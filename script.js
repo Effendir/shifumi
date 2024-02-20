53142fd07ed8ca774884dfd01db1ca1ef15ffd0c
eugene dami <eugene.dami@yahoo.fr>
2024-02-20 18:20:44 +0100

    Define a UI for the game

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let rockButton = document.querySelector(".rock");
+let paperButton = document.querySelector(".paper");
+let scissorsButton = document.querySelector(".scissors");
+let wrapper = document.querySelector(".wrapper");
+let resultMsg = document.querySelector(".result-msg");
+let score = document.querySelector(".score");
+
 function getComputerChoice() {
   let choices = ["Paper", "Rock", "Scissors"];
   let randomElement = choices[Math.floor(Math.random() * choices.length)];
@@ -5,39 +12,40 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-  let playerChoice = prompt("Paper, Rock or Scissors ? ").toLowerCase();
-  return playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
+  rockButton.addEventListener("click", () => {
+    rockPaperScissorsRound("Rock", getComputerChoice());
+  })
+  paperButton.addEventListener("click", () => {
+    rockPaperScissorsRound("Paper", getComputerChoice());
+  })
+  scissorsButton.addEventListener("click", () => {
+    rockPaperScissorsRound("Scissors", getComputerChoice());
+  })
 }
 
 function rockPaperScissorsRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    console.log("It's a tie!");
+    resultMsg.innerText = "It's a tie!";
   } else if (playerSelection === "Paper" && computerSelection === "Scissors" || playerSelection === "Rock" && computerSelection === "Paper" || playerSelection === "Scissors" && computerSelection === "Rock") {
-    console.log(`You lose, ${computerSelection} beats ${playerSelection}.`);
     computerScore++;
+    resultMsg.innerText = `You lose, ${computerSelection} beats ${playerSelection}.`;
   } else {
-    console.log(`You Win! ${playerSelection} beats ${computerSelection}.`);
     playerScore++;
+    resultMsg.innerText = `You Win! ${playerSelection} beats ${computerSelection}.`;
   }
-}
-
-function playGame(rounds) {
-  for (let i = 0; i < rounds; i++) {
-    rockPaperScissorsRound(getPlayerChoice(), getComputerChoice());
-  }
-
-  if (playerScore === computerScore) {
-    console.log("Win/win let's redo a match!");
-    playGame(5);
-  }
-  else if (playerScore < computerScore) {
-    console.log(`Winner is : Computer ${computerScore} / ${playerScore}`);
-  } else {
-    console.log(`Winner is : Player ${playerScore} / ${computerScore}`);
-  }
+  score.innerText = `Score : Player - ${playerScore} / Computer - ${computerScore}`;
+  checkScore(playerScore, computerScore);
 }
 
 let playerScore = 0;
 let computerScore = 0;
 
-playGame(5);
+function checkScore(player, computer) {
+  if (player === 5) {
+    score.innerText += " | You won the game with 5 points"
+  } else if (computer === 5) {
+    score.innerText += " | You lose that game, computer won with 5 points"
+  }
+}
+
+getPlayerChoice();
